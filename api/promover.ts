b95a583364fe8d3672bb2eb8e686d4dbfbc9020c
phillipji00 <phillipji00@gmail.com>
2025-06-17 09:02:43 -0300

    conserto promover

--- api/promover.ts
+++ api/promover.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { initializeDatabase, promoverCaso } from '../../lib/dbClient';
+import { initializeDatabase, promoverCaso } from '../lib/dbClient';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
